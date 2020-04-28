f9de51aa1b2dcfe3ff583265f7e178804b1d0c91
Harideep <hari.apssdc@gmail.com>
2020-04-28 20:35:22 +0530

    Updates

---
+++ precache-manifest.03945d0fdeb92c06c3a28e79da0de07e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cfee0d4e5fbe4eac5ffbd974dc4ca457",
+    "url": "/React/index.html"
+  },
+  {
+    "revision": "1878558418fb190038e9",
+    "url": "/React/static/css/main.1e187ebd.chunk.css"
+  },
+  {
+    "revision": "443c196c62caa0aeeadf",
+    "url": "/React/static/js/2.690de6e2.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/React/static/js/2.690de6e2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1878558418fb190038e9",
+    "url": "/React/static/js/main.b785118b.chunk.js"
+  },
+  {
+    "revision": "f23d8f58cb2d5ff738e6",
+    "url": "/React/static/js/runtime-main.311bb74a.js"
+  }
+]);
